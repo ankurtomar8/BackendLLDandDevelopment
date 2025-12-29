92d4b19dfe7a09aaba6cd746ccf5a29721651577
Dev-Ankur <ankurtomar8@outlook.com>
2025-12-29 09:02:09 +0530

    Client.java added

---
+++ src/main/java/Interfaces/Client.java
@@ -0,0 +1,24 @@
+package Interfaces;
+
+public class Client {
+    public static void main(String[] args){
+
+        ArrayStack arrayStack = new ArrayStack();
+
+        // interface type can refer to the object
+        // of any class which has implement the interface
+
+        Stack stack = new ArrayStack();
+        stack.push(10);
+
+        stack = new LinkedListStack();
+        stack.push(20);
+
+        stack = new QueueStack();
+        stack.push(30);
+
+
+
+
+    }
+}
