455a5c065b601aae1b2c8d4f1c2a3d555e849c24
Dev-Ankur <ankurtomar8@outlook.com>
2026-01-03 09:36:43 +0530

    Clothing.java added

---
+++ src/main/java/Milestone1/Clothing.java
@@ -0,0 +1,19 @@
+package Milestone1;
+
+public class Clothing extends Item {
+
+    String size;
+
+    public Clothing(String id, String name, double price, int quantity, String size) {
+        super(id, name, price, quantity);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
