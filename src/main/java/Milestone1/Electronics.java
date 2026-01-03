8344f5e7083f3c35298d1f34326d96ed0c7997b2
Dev-Ankur <ankurtomar8@outlook.com>
2026-01-03 09:36:52 +0530

    Electronics.java added

---
+++ src/main/java/Milestone1/Electronics.java
@@ -0,0 +1,18 @@
+package Milestone1;
+
+public class Electronics extends Item {
+   int warranty;
+
+    public int getWarranty() {
+        return warranty;
+    }
+
+    public void setWarranty(int warranty) {
+        this.warranty = warranty;
+    }
+
+    public Electronics(String id, String name, double price, int quantity, int warranty) {
+        super(id, name, price, quantity);
+        this.warranty = warranty;
+    }
+}
