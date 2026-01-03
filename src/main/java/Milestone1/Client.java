2a6119f056e05aea6235d27fb44734feee7331e3
Dev-Ankur <ankurtomar8@outlook.com>
2026-01-03 09:36:33 +0530

    Client.java added

---
+++ src/main/java/Milestone1/Client.java
@@ -0,0 +1,31 @@
+package Milestone1;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Client {
+    public static void main(String[] args) {
+        List<Item> items = new ArrayList<Item>();
+
+        items.add(new Electronics("E1","Laptop",54.5,5,5));
+        items.add(new Clothing("C1","T-shirt",44.64,4,"M"));
+        items.add(new Book("B1","Java Pro",24.5,3,"John Doe"));
+        System.out.println("Item List :  ");
+        for (Item item : items){
+            System.out.println(item.getName() + "  :  "+item.getPrice());
+        }
+
+        // sort this list
+        // Custom classess don't have anything sorting order
+
+        System.out.println("--------------------------------------------------------------");
+        Collections.sort(items);
+        for (Item item : items){
+            System.out.println(item.getName() + "  :  "+item.getPrice());
+        }
+
+    }
+}
